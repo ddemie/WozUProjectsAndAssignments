13e791045d96f1986366736e0da2eff6b9eda7dc
dallindemie <dallindemie@gmail.com>
2021-12-29 20:32:15 -0700

    WozU first commit. Includes SQL projets as well

---
+++ wozUTrainingProjects/src/handsOnAssignments/HandsOn.java
@@ -0,0 +1,24 @@
+package handsOnAssignments;
+
+public class HandsOn {
+
+	public static void main(String[] args) {
+		boolean isSunny = false;
+		boolean atBeach = true;
+		
+		if (isSunny == true) {
+			System.out.println("Wear sunglasses"); 
+		}
+		else {
+			System.out.println ("Don't need sunglasses.");
+			System.out.println ("Don't go to beach");
+		}
+		if (atBeach == true) {
+			System.out.println ("Wear sunblock!");
+			System.out.println ("Come back tomorrow.");
+			 }
+			else {
+				System.out.println ("Dont need sunblock");
+			}
+	}
+}
